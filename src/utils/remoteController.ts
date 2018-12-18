75a147f84413fd10dbbef70bfcf95252e3b9441f
phadyniak <public.hadyniak@gmail.com>
2018-12-18 19:16:15 +0200

    add turn server

--- src/utils/remoteController.ts
+++ src/utils/remoteController.ts
@@ -3,22 +3,32 @@ declare var Peer: any;
 type EventHandler = (data: any) => void;
 
 export default class RemoteController {
-    private eventListeners: {[eventType: string]: EventHandler[]};
+    private eventListeners: { [eventType: string]: EventHandler[] };
 
     constructor(id: string, options: any) {
-        this.eventListeners = {}; 
+        this.eventListeners = {};
         //
-        options = {...options, config: {
-            iceServers: [
-                { urls: 'stun:stun1.l.google.com:19302' },
-            ],
-        }};
+        options = {
+            ...options,
+            config: {
+                iceServers: [
+                    { urls: 'stun:stun1.l.google.com:19302' },
+                    {
+                        "urls": [
+                            "turn:13.250.13.83:3478?transport=udp"
+                        ],
+                        "username": "YzYNCouZM1mhqhmseWk6",
+                        "credential": "YzYNCouZM1mhqhmseWk6"
+                    },
+                ],
+            }
+        };
         const peer = new Peer(randomId(), options);
         const conn = peer.connect(id);
         conn.on('open', () => {
             this.__emit(`open`, {});
             //
-            conn.on('data', (data: {event: string, data: any}) => {
+            conn.on('data', (data: { event: string, data: any }) => {
                 // console.log('Received', data);
                 if (data.event) {
                     this.__emit(data.event, data.data);
@@ -39,7 +49,7 @@ export default class RemoteController {
     __emit(eventType: string, data: any) {
         this.__ensureListeners(eventType);
         for (const handler of this.eventListeners[eventType]) {
-            handler(Object.assign({ preventDefault() {}}, data));
+            handler(Object.assign({ preventDefault() { } }, data));
         }
     }
 
